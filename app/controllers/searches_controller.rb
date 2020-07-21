eff4049edf40dcb80409d85659dc1f76c5916510
Anne Costa <anneizaura@gmail.com>
2020-07-21 00:28:10 -0300

    Controller methods

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,12 @@
+class SearchesController < ApplicationController
+  def new
+
+  end
+
+  def show
+    search_term = params['q'].capitalize
+    response = RestClient.get "https://api.github.com/search/repositories?q=language:#{search_term}"
+    @json = JSON.parse response
+
+  end
+end
