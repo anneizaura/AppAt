fbaa8f882460d33a24a864d969d263e41d8fb05f
Anne Costa <anneizaura@gmail.com>
2020-07-22 01:23:48 -0300

    Config Front-End

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # QUERIA USAR RESOURCES ACHO QUE TENTEI DA FORMA ERRADA
-  # PS: AINDA NAO CRIEI MODELS NEM MIGRATIONS E NECESSÁRIO PARA SALVAR NO DB?
-  # get '/search', to: 'searches#create'
   get '/search', to: 'searches#new'
   post '/search', to: 'searches#show'
 end
